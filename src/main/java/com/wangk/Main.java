90451e424835095d4ae8b628a54a8d7c8759c5b6
MissTheSky <MissTheSky@gmail.com>
2023-05-25 11:44:05 +0800

    修改主分支代码

--- src/main/java/com/wangk/Main.java
+++ src/main/java/com/wangk/Main.java
@@ -8,7 +8,5 @@ package com.wangk;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
-        System.out.println("添加主分支代码");
-        System.out.println("修改测试分支代码");
     }
 }
