06d0590c1ad9ce3ec97ad6b45998ecb607e72294
MissTheSky <MissTheSky@gmail.com>
2023-05-25 09:59:22 +0800

    first commit

---
+++ src/main/java/com/wangk/Main.java
@@ -0,0 +1,12 @@
+package com.wangk;
+
+/**
+ * @create: 2023-05-25 9:41
+ * @author: 怀念天空
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
