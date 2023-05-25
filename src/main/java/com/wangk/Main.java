38c6ff05ce186cd0cd4c71adc2e1383327d51fdd
MissTheSky <MissTheSky@gmail.com>
2023-05-25 10:37:19 +0800

    test first commit

--- src/main/java/com/wangk/Main.java
+++ src/main/java/com/wangk/Main.java
@@ -3,10 +3,11 @@ package com.wangk;
 /**
  * @create: 2023-05-25 9:41
  * @author: 怀念天空
- * @Description:
+ * @Description: git 测试
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("添加测试代码");
     }
 }
