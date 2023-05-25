d58e5c4966790c577dd1ccc4b45af89fff687364
MissTheSky <MissTheSky@gmail.com>
2023-05-25 13:10:32 +0800

    修改主分支代码

--- src/main/java/com/wangk/Main.java
+++ src/main/java/com/wangk/Main.java
@@ -7,6 +7,6 @@ package com.wangk;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,Git!");
+        System.out.println("Hello,git!");
     }
 }
